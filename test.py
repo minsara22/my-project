e538cd569aa68f0ee5bc1569df464741e3a43bd1
minsara22 <minsara2001@gmail.com>
2025-03-25 15:06:59 +0530

    Added test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Print a message
+print("Hello, GitHub!")
+
+# Print the current date and time
+now = datetime.datetime.now()
+print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
