4565cf96d26982188f8f4916aacf059daea49879
Dineth0627 <akashhpd.22@uom.lk>
2025-03-25 20:04:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ print("Hello, GitHub!")
 # Print the current date and time
 now = datetime.datetime.now()
 print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+a=10
+b=20
+
+print(a+b)
